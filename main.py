d977d9e12792803bad3f683346c34074668f93bc
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2026-09-20 17:22:08 +0300

    History

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai.chat_models import HumanMessage,AIMessage
+from langchain_core.chat_history import BaseChatMessageHistory,InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 
 load_dotenv()
 
@@ -9,12 +12,31 @@ model = ChatGoogleGenerativeAI(
     temperature=0.7
 )
 
-if __name__ == "__main__":
-    messages = [
-        HumanMessage(content="Hello, my name is Berkay"),
-        AIMessage(content="Hello Berkay, how can i help you today?"),
-        HumanMessage(content="What is my name?"),
-    ]
-    response = model.invoke(messages)
-    print(response.content)
+store = {}
 
+def get_session_history(session_id:str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant.Asnwer all questions to the best of your ability"),
+        MessagesPlaceholder(variable_name="messages")
+    ]
+)
+
+chain = prompt | model
+config ={"configurable": {"session_id":"666"}}
+with_message_history = RunnableWithMessageHistory(chain,get_session_history)
+
+if __name__ == "__main__":
+    while True:
+        user_input = input(">")
+        response = with_message_history.invoke(
+            [
+                HumanMessage(content=user_input)
+            ],
+            config=config
+        )
+        print(response.content)
